1cac8f5fef7156f113fcd59a416bc5d704e785ad
2xCO <corinna.kunst@icloud.com>
2024-12-22 15:18:43 +0100

    serverlocal verknüpfung github und 11ty build funktioniert wieder?

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,14 @@
 module.exports = function (eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy("js");
-    eleventyConfig.addPassthroughCopy("**/*.css");
+    // eleventyConfig.addPassthroughCopy("**/*.css");
     eleventyConfig.addPassthroughCopy("**/*.woff2");
     eleventyConfig.addPassthroughCopy("**/*.zip");
     eleventyConfig.addPassthroughCopy("**/*.jpg");
     eleventyConfig.addPassthroughCopy("**/*.png");
-    eleventyConfig.addPassthroughCopy("assets/.css");
+    eleventyConfig.addPassthroughCopy("assets/css");
+    eleventyConfig.addPassthroughCopy("assets/img");
+    
 
 
 };
